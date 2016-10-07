c5d5451efa18d5321867caf299f85dd18afb9c31
metalhead_6666 <noe.godinho.92@gmail.com>
2016-10-07 17:07:26 +0100

    Marshal addition

--- Projeto 1/src/Main.java
+++ Projeto 1/src/Main.java
@@ -1,6 +1,11 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.File;
 import java.io.IOException;
 
 public class Main{
@@ -9,7 +14,7 @@ public class Main{
 		Document doc;
 		Elements links;
 
-		try {
+		try{
 			doc = Jsoup.connect(websiteToParse).get();
 			System.out.println(doc.title() + "\n");
 			links = doc.getElementsByTag("tr");
@@ -40,5 +45,18 @@ public class Main{
 		}catch(IOException ioe){
 			ioe.printStackTrace();
 		}
-	}	
+	}
+
+	public void createMarshal(Olympics olympics){
+		try{
+			File file = new File("xml/medals.xml");
+			JAXBContext jaxbContext = JAXBContext.newInstance(Olympics.class);
+			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			jaxbMarshaller.marshal(olympics, file);
+		}catch(JAXBException jaxbe){
+			jaxbe.printStackTrace();
+		}
+	}
 }
