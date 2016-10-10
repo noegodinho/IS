68f031979c4dff35ee8b8030f0dd649f66810600
Noé Godinho <noe.godinho.92@gmail.com>
2016-10-11 00:33:08 +0100

    JMS Topic receiver created

--- Projeto 1/src/HTMLSummary.java
+++ Projeto 1/src/HTMLSummary.java
@@ -1,3 +1,6 @@
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
@@ -5,29 +8,54 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.io.StringReader;
 
-/**
- * Created by Rocha on 08/10/16.
- */
-public class HTMLSummary {
+public class HTMLSummary{
     public static void main(String[] args){
-        try
-        {
-            TransformerFactory tFactory = TransformerFactory.newInstance();
+        try{
+            while(true){
+                Receiver r = new Receiver();
+                String xml = r.receive();
+                System.out.println("Message: " + xml);
 
-            Source xslDoc = new StreamSource("xml/medals.xsl");
-            Source xmlDoc = new StreamSource("xml/medals.xml");
+                TransformerFactory tFactory = TransformerFactory.newInstance();
 
-            String outputFileName = "xml/medals.html";
-            OutputStream htmlFile = new FileOutputStream(outputFileName);
+                Source xslDoc = new StreamSource("xml/medals.xsl");
+                StringReader xmlD = new StringReader(xml);
+                Source xmlDoc = new StreamSource(xmlD);
 
-            Transformer transformer = tFactory.newTransformer(xslDoc);
-            transformer.transform(xmlDoc, new StreamResult(htmlFile));
-            System.out.println("HTML file created successfully");
-        }
-        catch(Exception e)
-        {
+                String outputFileName = "xml/medals.html";
+                OutputStream htmlFile = new FileOutputStream(outputFileName);
+
+                Transformer transformer = tFactory.newTransformer(xslDoc);
+                transformer.transform(xmlDoc, new StreamResult(htmlFile));
+                System.out.println("HTML file created successfully");
+            }
+        }catch(Exception e){
             e.printStackTrace();
+        }
+    }
+
+    public static class Receiver{
+        private ConnectionFactory cf;
+        private Destination d;
+
+        public Receiver() throws NamingException{
+            this.cf = InitialContext.doLookup("jms/RemoteConnectionFactory");
+            this.d = InitialContext.doLookup("jms/topic/XMLTopic");
+        }
+
+        private String receive(){
+            String msg = null;
+
+            try(JMSContext jcontex = cf.createContext("Is", "isisisis")) {
+                JMSConsumer mc = jcontex.createConsumer(d);
+                msg = mc.receiveBody(String.class);
+            } catch (JMSRuntimeException re) {
+                re.printStackTrace();
+            }
+
+            return msg;
         }
     }
 }
