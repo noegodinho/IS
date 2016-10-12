ebb77961d17915ceb478a8c12e9640f2c529d893
Noé Godinho <noe.godinho.92@gmail.com>
2016-10-12 01:45:30 +0100

    Implemented temporary queue creation and reception of response

--- Projeto 1/src/MedalsRequester.java
+++ Projeto 1/src/MedalsRequester.java
@@ -1,3 +1,5 @@
+import sun.security.krb5.internal.crypto.Des;
+
 import javax.jms.*;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -16,15 +18,24 @@ public class MedalsRequester{
 
     public MedalsRequester() throws NamingException{
         this.cf = InitialContext.doLookup("jms/RemoteConnectionFactory");
-        this.d = InitialContext.doLookup("jms/queue/KeeperReceiver");
+        this.d = InitialContext.doLookup("jms/queue/KeeperRequester");
         send("USA");
     }
 
     private void send(String text){
         try(JMSContext jcontext = cf.createContext("Is", "isisisis")){
             JMSProducer mp = jcontext.createProducer();
-            mp.send(d, text);
-        }catch (JMSRuntimeException re){
+            TextMessage txt = jcontext.createTextMessage();
+            Destination reply = jcontext.createTemporaryQueue();
+            txt.setJMSReplyTo(reply);
+            txt.setText(text);
+
+            mp.send(d, txt);
+
+            JMSConsumer jms = jcontext.createConsumer(reply);
+            TextMessage tm = (TextMessage)jms.receive();
+            System.out.println("Reply: " + tm.getText());
+        }catch (JMSRuntimeException | JMSException re){
             re.printStackTrace();
         }
     }
