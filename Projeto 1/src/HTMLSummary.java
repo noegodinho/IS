587795e16ef4a618598fc689c5f822d1445333cf
Noé Godinho <noe.godinho.92@gmail.com>
2016-10-12 01:13:38 +0100

    Minor changes

--- Projeto 1/src/HTMLSummary.java
+++ Projeto 1/src/HTMLSummary.java
@@ -17,71 +17,69 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.StringReader;
 
-public class HTMLSummary{
+public class HTMLSummary implements MessageListener{
+    private ConnectionFactory cf;
+    private Destination d;
+
     public static void main(String[] args){
         try{
-            HTMLSummaryCreator html = new HTMLSummaryCreator();
-            html.launchAndWait();
+            new HTMLSummary();
         }catch(NamingException ne){
             ne.printStackTrace();
         }
     }
 
-    public static class HTMLSummaryCreator implements MessageListener{
-        private ConnectionFactory cf;
-        private Destination d;
+    public HTMLSummary() throws NamingException{
+        this.cf = InitialContext.doLookup("jms/RemoteConnectionFactory");
+        this.d = InitialContext.doLookup("jms/topic/XMLTopic");
+        this.launchAndWait();
+    }
 
-        public HTMLSummaryCreator() throws NamingException{
-            this.cf = InitialContext.doLookup("jms/RemoteConnectionFactory");
-            this.d = InitialContext.doLookup("jms/topic/XMLTopic");
+    @Override
+    public void onMessage(Message msg){
+        try{
+            TextMessage tmsg = (TextMessage) msg;
+            String xml = tmsg.getText();
+
+            TransformerFactory tFactory = TransformerFactory.newInstance();
+
+            Source xmlDocToValidate = new StreamSource(new StringReader(xml));
+            Source xmlDoc = new StreamSource(new StringReader(xml));
+
+            Source xsdFile = new StreamSource("xml/medals.xsd");
+
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = schemaFactory.newSchema(xsdFile);
+            Validator validator = schema.newValidator();
+
+            String outputFileName = "xml/medals" + System.currentTimeMillis() + ".html";
+            OutputStream htmlFile = new FileOutputStream(outputFileName);
+
+            Source xslDoc = new StreamSource("xml/medals.xsl");
+
+            validator.validate(xmlDocToValidate);
+
+            Transformer transformer = tFactory.newTransformer(xslDoc);
+            transformer.transform(xmlDoc, new StreamResult(htmlFile));
+
+            System.out.println("HTML file created successfully");
+        }catch(SAXException saxe){
+            saxe.printStackTrace();
+            System.out.println("Message is NOT a valid XML message");
+        }catch (Exception e){
+            e.printStackTrace();
         }
+    }
 
-        @Override
-        public void onMessage(Message msg){
-            try{
-                TextMessage tmsg = (TextMessage) msg;
-                String xml = tmsg.getText();
+    private void launchAndWait(){
+        try(JMSContext jcontext = cf.createContext("Is", "isisisis")){
+            jcontext.setClientID("summary");
+            JMSConsumer mc = jcontext.createDurableConsumer((Topic) d, "summary");
+            mc.setMessageListener(this);
 
-                TransformerFactory tFactory = TransformerFactory.newInstance();
-
-                Source xmlDocToValidate = new StreamSource(new StringReader(xml));
-                Source xmlDoc = new StreamSource(new StringReader(xml));
-
-                Source xsdFile = new StreamSource("xml/medals.xsd");
-
-                SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-                Schema schema = schemaFactory.newSchema(xsdFile);
-                Validator validator = schema.newValidator();
-
-                String outputFileName = "xml/medals" + System.currentTimeMillis() + ".html";
-                OutputStream htmlFile = new FileOutputStream(outputFileName);
-
-                Source xslDoc = new StreamSource("xml/medals.xsl");
-
-                validator.validate(xmlDocToValidate);
-
-                Transformer transformer = tFactory.newTransformer(xslDoc);
-                transformer.transform(xmlDoc, new StreamResult(htmlFile));
-
-                System.out.println("HTML file created successfully");
-            }catch(SAXException saxe){
-                saxe.printStackTrace();
-                System.out.println("Message is NOT a valid XML message");
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-
-        public void launchAndWait(){
-            try(JMSContext jcontext = cf.createContext("Is", "isisisis")){
-                jcontext.setClientID("summary");
-                JMSConsumer mc = jcontext.createDurableConsumer((Topic) d, "summary");
-                mc.setMessageListener(this);
-
-                System.in.read();
-            }catch (JMSRuntimeException | IOException re){
-                re.printStackTrace();
-            }
+            System.in.read();
+        }catch (JMSRuntimeException | IOException re){
+            re.printStackTrace();
         }
     }
 }
--- Projeto 1/src/MedalsRequester.java
+++ Projeto 1/src/MedalsRequester.java
@@ -3,31 +3,29 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 public class MedalsRequester{
+    private ConnectionFactory cf;
+    private Destination d;
+
     public static void main(String[] args){
         try{
-            new Sender();
+            new MedalsRequester();
         }catch(NamingException ne){
             ne.printStackTrace();
         }
     }
 
-    public static class Sender{
-        private ConnectionFactory cf;
-        private Destination d;
+    public MedalsRequester() throws NamingException{
+        this.cf = InitialContext.doLookup("jms/RemoteConnectionFactory");
+        this.d = InitialContext.doLookup("jms/queue/KeeperReceiver");
+        send("USA");
+    }
 
-        public Sender() throws NamingException{
-            this.cf = InitialContext.doLookup("jms/RemoteConnectionFactory");
-            this.d = InitialContext.doLookup("jms/queue/KeeperReceiver");
-            send("USA");
-        }
-
-        private void send(String text){
-            try(JMSContext jcontext = cf.createContext("Is", "isisisis")){
-                JMSProducer mp = jcontext.createProducer();
-                mp.send(d, text);
-            }catch (JMSRuntimeException re){
-                re.printStackTrace();
-            }
+    private void send(String text){
+        try(JMSContext jcontext = cf.createContext("Is", "isisisis")){
+            JMSProducer mp = jcontext.createProducer();
+            mp.send(d, text);
+        }catch (JMSRuntimeException re){
+            re.printStackTrace();
         }
     }
 }
