0cebf7f1ef658aa0d6cd44882503cbc0686e76a4
Noé Godinho <noe.godinho.92@gmail.com>
2016-11-06 02:08:19 +0000

    Courses table

---
+++ Projeto 2/jpa-module/src/data/Course.java
@@ -0,0 +1,67 @@
+package data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+public class Course implements Serializable{
+    private static final long serialVersionUID = 5L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String courseName;
+
+    @ManyToOne
+    private Professor professor;
+
+    @ManyToMany
+    private List<Student> students;
+
+    @OneToMany(mappedBy = "course")
+    private List<Material> materials;
+
+    public Course(){
+        super();
+    }
+
+    public Course(String courseName, Professor professor, List<Student> students){
+        super();
+        this.courseName = courseName;
+        this.professor = professor;
+        this.students = students;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public List<Material> getMaterials() {
+        return materials;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public void setMaterials(List<Material> materials) {
+        this.materials = materials;
+    }
+}
