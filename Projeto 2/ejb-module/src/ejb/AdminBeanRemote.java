6b830590341af15bace2dcdabdee3e36996d414f
Noé Godinho <noe.godinho.92@gmail.com>
2016-11-10 00:07:03 +0000

    Interface for AdminBean EJB

---
+++ Projeto 2/ejb-module/src/ejb/AdminBeanRemote.java
@@ -0,0 +1,25 @@
+package ejb;
+
+import data.Course;
+
+import javax.ejb.Remote;
+import java.util.Date;
+import java.util.List;
+
+@Remote
+public interface AdminBeanRemote{
+    boolean createStudentAccount(String hashedPassword, String name, Date birth, String instEmail,
+                                 String altEmail, String address, Integer telephone, Integer number,
+                                 Integer yearOfCourse);
+    boolean createProfessorAccount(String hashedPassword, String name, Date birth, String instEmail,
+                                   String altEmail, String address, Integer telephone, Integer internalNumber,
+                                   String category, String office, Integer internalTelephoneNumber, double salary);
+    boolean editStudent(String hashedPassword, String name, Date birth, String instEmail, String altEmail,
+                        String address, Integer telephone, Integer number, Integer yearOfCourse, List<Course> courses,
+                        String newInstEmail);
+    boolean editProfessor(String hashedPassword, String name, Date birth, String instEmail,
+                          String altEmail, String address, Integer telephone, Integer internalNumber,
+                          String category, String office, Integer internalTelephoneNumber, double salary,
+                          List<Course> courses, String newInstEmail);
+    boolean deleteUser(String instEmail);
+}
