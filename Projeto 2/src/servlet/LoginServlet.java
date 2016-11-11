4f5c6c64f8d37b87949d12883ebda6d195d00810
Fernando Rocha <fmrocha@student.dei.uc.pt>
2016-11-11 01:29:48 +0000

    Login da piça

---
+++ Projeto 2/src/servlet/LoginServlet.java
@@ -0,0 +1,116 @@
+package servlet;
+
+import data.User;
+import ejb.ClientBean;
+import ejb.ClientBeanRemote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+
+@WebServlet(name = "/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @EJB
+    ClientBeanRemote ejbremote;
+    Logger logger;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet(){
+        super();
+        logger = LoggerFactory.getLogger(ClientBean.class);
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String instEmail = request.getParameter("instEmail");
+        String password = request.getParameter("password");
+        PrintWriter out = response.getWriter();
+        ArrayList<String> menuOptions = new ArrayList<>();
+
+        if(instEmail.isEmpty() || password.isEmpty()){
+            out.println("<script type=\"text/javascript\">");
+            out.println("if (confirm(\"Need to fill all the fields!\")) {x=\"coisas\";}");
+            out.println("window.location.replace(\"http://localhost:8080/web/index.jsp\");");
+            out.println("</script>");
+            logger.error("User did not input email and/or password");
+        }
+
+        else{
+            try{
+                User loggedUser = ejbremote.loginUser(instEmail, password);
+                if(loggedUser != null){
+                    request.setAttribute("user", loggedUser);
+                    session.setAttribute("user", loggedUser);
+                    setOptions(menuOptions,loggedUser.getUserType());
+                    session.setAttribute("options", menuOptions);
+                    request.getRequestDispatcher("menu.jsp").forward(request, response);
+                }
+                else{
+                    out.println("<script type=\"text/javascript\">");
+                    out.println("if (confirm(\"Wrong password!\")) {x=\"coisas\";}");
+                    out.println("window.location.replace(\"http://localhost:8080/web/index.jsp\");");
+                    out.println("</script>");
+                    logger.error("User input wrong password");
+                }
+
+            }catch(EJBException ejbe){
+                out.println("<script type=\"text/javascript\">");
+                out.println("if (confirm(\"No account found related to this email!!\")) {x=\"coisas\";}");
+                out.println("window.location.replace(\"http://localhost:8080/web/index.jsp\");");
+                out.println("</script>");
+                logger.error("User does not exist");
+
+            }
+        }
+    }
+
+    //If userType remains int
+    private void setOptions(ArrayList<String> menuOptions, int userType ){
+        switch(userType){
+            case 0: //admin
+                menuOptions.add("Create New User");
+                menuOptions.add("Edit User Information");
+                menuOptions.add("Create New Course");
+                menuOptions.add("Edit Course Information");
+                menuOptions.add("Delete User");
+                menuOptions.add("Delete Course");
+            case 1: //prof
+                menuOptions.add("Upload Course Material");
+                menuOptions.add("Delete Material");
+                menuOptions.add("List Students");
+                menuOptions.add("Search Student");
+            case 2: //student
+                menuOptions.add("List Course");
+                menuOptions.add("List Materials");
+                break;
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.error("NAO ENCONTRA POST");
+        if(request.getParameter("action").equals("login"))
+            processRequest(request, response);
+    }
+}
--- Projeto 2/web/index.jsp
+++ Projeto 2/web/index.jsp
@@ -11,6 +11,17 @@
     <title>$Title$</title>
   </head>
   <body>
-  $END$
+  <form action="./LoginServlet" method="POST">
+    <font face="verdana" color="white">
+      <section align="center">
+        <div class="login">
+          <h1>Login to Web App</h1>
+          <p><input type="text" name="instEmail" placeholder="Email" style="text-align: center; font-size:14pt; width: 350px; height: 30px; "> </p>
+          <p><input type="password" name="password" placeholder="Password" style="text-align: center; font-size:14pt; width: 350px; height: 30px "></p>
+          <p class="submit"><input type="submit" name="action" value="login" style="width: 150px; height: 50px"></p>
+        </div>
+      </section>
+    </font>
+  </form>
   </body>
 </html>
