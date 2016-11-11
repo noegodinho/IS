5c4f2cc8eec54a799053ac2c3653649b3b56eff9
Noé Godinho <noe.godinho.92@gmail.com>
2016-11-11 14:30:14 +0000

    Register Servlet

---
+++ Projeto 2/src/servlet/RegisterServlet.java
@@ -0,0 +1,56 @@
+package servlet;
+
+import ejb.AdminBean;
+import ejb.AdminBeanRemote;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet{
+    private static final long serialVersionUID = 1L;
+
+    @EJB
+    private AdminBeanRemote ejbremote;
+    private Logger logger;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterServlet(){
+        this.logger = LoggerFactory.getLogger(AdminBean.class);
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        HttpSession session = request.getSession();
+        PrintWriter out = response.getWriter();
+        String password = request.getParameter("password");
+
+        String hashedPassword = new LoginServlet().createHash(password);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        if(request.getParameter("action").equals("register")){
+            processRequest(request, response);
+        }
+    }
+}
