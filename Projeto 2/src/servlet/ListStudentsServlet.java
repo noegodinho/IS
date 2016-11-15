56cc45f76cf1d47678a2e1c9d49e49f25ec7e556
Fernando Rocha <fmrocha@student.dei.uc.pt>
2016-11-15 00:22:36 +0000

    Create Course

--- Projeto 2/src/servlet/ListStudentsServlet.java
+++ Projeto 2/src/servlet/ListStudentsServlet.java
@@ -5,13 +5,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 
 @WebServlet(name = "/ListStudentsServlet")
 public class ListStudentsServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        HttpSession session = request.getSession();
+        if(request.getParameter("action").equals("logout")){
+            session.removeAttribute("user");
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- Projeto 2/src/servlet/NewCourseServlet.java
+++ Projeto 2/src/servlet/NewCourseServlet.java
@@ -27,13 +27,14 @@ public class NewCourseServlet extends HttpServlet {
     private AdminBeanRemote ejbremote;
     private ProfessorDTO choosenProfessor;
     private List<StudentDTO> students;
-    private List<StudentDTO> courseStudents;
+    private List<String> courseStudents;
 
     Logger logger;
 
     public NewCourseServlet(){
         super();
         this.courseStudents = new ArrayList<>();
+        this.students = new ArrayList<>();
         this.logger = LoggerFactory.getLogger(AdminBean.class);
     }
 
@@ -43,24 +44,26 @@ public class NewCourseServlet extends HttpServlet {
         String courseName = request.getParameter("courseName");
         String profEmail = request.getParameter("profEmail");
 
-        List<ProfessorDTO> professorsDTO = this.ejbremote.getProfessors();
+        /*List<ProfessorDTO> professorsDTO = this.ejbremote.getProfessors();
 
         for (ProfessorDTO professor : professorsDTO){
             if (professor.getInstEmail().equals(profEmail))
                 choosenProfessor = professor;
-        }
+        }*/
 
-        if (!courseName.isEmpty() && choosenProfessor != null && !courseStudents.isEmpty()){
-            this.ejbremote.createCourse(courseName, choosenProfessor,courseStudents);
+        //if (!courseName.isEmpty() && choosenProfessor != null && !courseStudents.isEmpty()){
+        if (!courseName.isEmpty() && !profEmail.isEmpty() && !courseStudents.isEmpty()){
+            this.ejbremote.createCourse(courseName, profEmail, courseStudents);
             logger.info("Course successfully created");
             request.getRequestDispatcher("menu.jsp").forward(request, response);
         }
 
         else {
 
-            if (choosenProfessor == null) {
+            if (profEmail.isEmpty()) {
                 out.println("<script type=\"text/javascript\">");
                 out.println("if (confirm(\"Professor doesn't exist\")) {}");
+                out.println("window.location.replace(\"http://localhost:8080/Web/newCourse.jsp\");");
                 out.println("</script>");
                 logger.error("Invalid Professor");
                 request.getRequestDispatcher("newCourse.jsp").forward(request, response);
@@ -69,36 +72,42 @@ public class NewCourseServlet extends HttpServlet {
             else if (courseStudents.isEmpty()) {
                 out.println("<script type=\"text/javascript\">");
                 out.println("if (confirm(\"Add at least one student\")) {}");
+                out.println("window.location.replace(\"http://localhost:8080/Web/newCourse.jsp\");");
                 out.println("</script>");
                 logger.error("Add at least one student");
                 request.getRequestDispatcher("newCourse.jsp").forward(request, response);
             }
         }
 
+        courseStudents.clear();
 
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
-        this.students = this.ejbremote.getStudents();
         if(request.getParameter("action").equals("create")){
             processRequest(request, response);
         }
 
         if(request.getParameter("action").equals("add")) {
             addStudentToList(request, response);
-            session.setAttribute("students",courseStudents);
+            session.setAttribute("students",students);
             request.getRequestDispatcher("newCourse.jsp").forward(request, response);
+        }
+
+        else if(request.getParameter("action").equals("logout")){
+            session.removeAttribute("user");
+            request.getRequestDispatcher("index.jsp").forward(request, response);
         }
     }
 
     private void addStudentToList(HttpServletRequest request, HttpServletResponse response) {
         String studentEmail = request.getParameter("studentEmail");
-        for (StudentDTO student : students){
+        courseStudents.add(studentEmail);
+        for (StudentDTO student : this.ejbremote.getStudents()){
             if (student.getInstEmail().equals(studentEmail))
-                courseStudents.add(student);
+                students.add(student);
         }
-
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
